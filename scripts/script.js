2138311de7e0a9bf63a2e122d70d7a8cbe1b2d57
Finn Builder <finn.builder@krieger613.com>
2023-07-26 13:51:13 -0400

    Version 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,23 @@
-function pick7(){
-  const picker = 0;
-
-  for(){
-    
+function playLotto() {
+  let htmlLotto = "";
+  let lottoNumbers = [];
+  while (lottoNumbers.length < 7) {
+    let pick = generateNumber();
+    if (lottoNumbers.indexOf(pick) === -1) {
+      lottoNumbers.push(pick);
+    }
   }
+  lottoNumbers.sort((a, b) => a - b); // Sort the array in ascending order
+
+  lottoNumbers.forEach((value, i) => {
+    htmlLotto += `<br><button class='js-number'>Pick ${i + 1}<br><div class="number">${value}</div></button>`;
+  });
+  return (document.querySelector("div").innerHTML = htmlLotto);
+}
+function generateNumber() {
+  let pick = 0;
+  while (pick === 0 || pick > 50) {
+    pick = Math.round(Math.random().toFixed(2) * 100);
+  }
+  return pick;
 }
